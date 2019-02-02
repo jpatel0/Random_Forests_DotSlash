ccf8812aaa1b2321f2332a4ba51bbfceedc762c0
Darsh Mistry <1111darsh@gmail.com>
2019-02-02 12:28:03 +0530

    h
    h

---
+++ app/src/main/java/com/randomforests/saaransh/NotesDashboard.java
@@ -0,0 +1,13 @@
+package com.randomforests.saaransh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotesDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_dashboard);
+    }
+}
