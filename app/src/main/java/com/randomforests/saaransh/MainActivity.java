c6f489b10210ea5608b28af8a097c48d36a36d0f
Darsh Mistry <1111darsh@gmail.com>
2019-02-02 12:28:58 +0530

    h
    h

--- app/src/main/java/com/randomforests/saaransh/MainActivity.java
+++ app/src/main/java/com/randomforests/saaransh/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        floatingActionButton=(FloatingActionButton)floatingActionButton(R.id.floatingActionButton);
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
