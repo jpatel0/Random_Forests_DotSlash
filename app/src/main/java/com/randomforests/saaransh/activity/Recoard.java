7a0c812d8e402c5f509271e0c28efb03a6f9d5cd
Darsh Mistry <1111darsh@gmail.com>
2019-02-02 15:58:06 +0530

    timer
    record timer

--- app/src/main/java/com/randomforests/saaransh/activity/Recoard.java
+++ app/src/main/java/com/randomforests/saaransh/activity/Recoard.java
@@ -3,6 +3,7 @@ package com.randomforests.saaransh.activity;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.randomforests.saaransh.R;
@@ -12,30 +13,33 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 public class Recoard extends AppCompatActivity {
 
+    String TAG="Recoard";
     public TextView time;
+    long  s =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recoard);
         time=(TextView)findViewById(R.id.texttime);
+
         new CountDownTimer(600000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+5:30"));
-                Date currentLocalTime = cal.getTime();
-                DateFormat date = new SimpleDateFormat("HH:mm:ss");
-                // you can get seconds by adding  "...:ss" to it
-                date.setTimeZone(TimeZone.getTimeZone("GMT+5:30"));
-
-                String localTime = date.format(currentLocalTime);
 
 
-                /*String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-                Log.d(TAG, "onCreate: " + currentDateTimeString);*/
-                time.setText(localTime); // This is your code
+
+
+                //long s=millisUntilFinished;
+                s=s+1000;
+
+                Log.d(TAG, "onTick: "+s);
+
+                String hms = String.format("%02d:%02d",  TimeUnit.MILLISECONDS.toMinutes(s) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(s)), TimeUnit.MILLISECONDS.toSeconds(s) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(s)));
+                time.setText(hms); // This is your code
             }
 
             public void onFinish() {
