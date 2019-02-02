8987efed7fd352bd2db2626b32ecd50be809d31d
Darsh Mistry <1111darsh@gmail.com>
2019-02-02 13:29:46 +0530

    h
    h

--- app/src/main/java/com/randomforests/saaransh/RecyclerViewAdapter.java
+++ app/src/main/java/com/randomforests/saaransh/RecyclerViewAdapter.java
@@ -1,17 +1,68 @@
 package com.randomforests.saaransh;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerViewAdapter extends RecyclerView.ViewHolder {
-    public TextView mTextView;
-    public RecyclerViewAdapter(TextView v) {
-        super(v);
-        mTextView = v;
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    ArrayList<String> list ;
+    Context context;
+    public RecyclerViewAdapter(Context context, ArrayList<String> list) {
+        this.list=list;
+        this.context=context;
     }
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_layout,viewGroup,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.textView.setText(list.get(i));
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(context,NotesDashboard.class);
+
+                context.startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+        LinearLayout parentLayout;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            textView=(TextView)itemView.findViewById(R.id.texview);
+            parentLayout=itemView.findViewById(R.id.Parent_layout);
+        }
+    }
 }
